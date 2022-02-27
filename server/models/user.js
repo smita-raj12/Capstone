cc175216eac3f2bd80c81341cb1181b145428ffb
smita <smita.raj12@gmail.com>
2022-02-27 14:58:45 -0800

    user authentication started

--- server/models/user.js
+++ server/models/user.js
@@ -1,45 +1,46 @@
 const config = require("config");
 const jwt = require("jsonwebtoken");
 const Joi = require("joi");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 50
-  },
-  email: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 255,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 1024
-  },
-  isAdmin: Boolean
-});
+// const userSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//     minlength: 2,
+//     maxlength: 50
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     minlength: 5,
+//     maxlength: 255,
+//     unique: true
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     minlength: 5,
+//     maxlength: 1024
+//   },
+//   isAdmin: Boolean
+// });
 
-userSchema.methods.generateAuthToken = function() {
+function generateAuthToken (user) {
+  //console.log("generate token",user)
   const token = jwt.sign(
     {
-      _id: this._id,
-      name: this.name,
-      email: this.email,
-      isAdmin: this.isAdmin
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      //isAdmin: this.isAdmin
     },
     config.get("jwtPrivateKey")
   );
   return token;
 };
 
-const User = mongoose.model("User", userSchema);
+//const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
   const schema = {
@@ -61,5 +62,5 @@ function validateUser(user) {
   return Joi.validate(user, schema);
 }
 
-exports.User = User;
+exports.generateAuthToken = generateAuthToken;
 exports.validate = validateUser;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,7 @@
 const auth = require("../middleware/auth");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
-const { User, validate } = require("../models/user");
+const {  validate, generateAuthToken } = require("../models/user");
 const express = require("express");
 const router = express.Router();
 const db = require ("../startup/db");
@@ -25,24 +25,24 @@ router.post("/", async (req, res) => {
     { getUser = "SELECT * FROM users WHERE email = ?;"}
     db.query(getUser, [email, password, name], (err, result)=>{
       console.log(err);
-      if (result) return res.status(400).send("User already registered.");
+      if (!result) return res.status(400).send("User already registered.");
     }); 
 
-    
+    const user = {name:name, email:email,password:password}
 
     // const user = new User(_.pick(req.body, ["name", "email", "password"]));
-    // const salt = await bcrypt.genSalt(10);
-    // user.password = await bcrypt.hash(user.password, salt);
-
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    //console.log("hashed password",user.password);
     insertUser = "INSERT INTO users (email, password, name) VALUES (?,?,?);"
-    db.query(insertUser, [email, password, name], (err, result)=>{
+    db.query(insertUser,[user.email, user.password, user.name], (err, result)=>{
       console.log(err);
     }); 
 
-  // const token = user.generateAuthToken();
-  // res
-  //   .header("x-auth-token", token)
-  //   .send(_.pick(user, ["_id", "name", "email"]));
+  const token = generateAuthToken(user);
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
