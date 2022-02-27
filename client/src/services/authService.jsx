0a74d47e67f4cdb89e4435e33da47f9e8a0fc64a
smita <smita.raj12@gmail.com>
2022-02-26 21:09:08 -0800

    add register and login form

---
+++ client/src/services/authService.jsx
@@ -0,0 +1,44 @@
+import http from "./httpServices";
+import jwt_decode from "jwt-decode";
+// import httpServices from "./httpServices";
+// import { apiUrl } from "../config.json";
+
+//http.setJwt(getjwt());
+//const apiEndpoint = apiUrl + "/auth";
+const apiEndpoint = "/auth";
+
+export async function login(email, password) {
+  const { data: jwt } = await http.post(apiEndpoint, { email, password });
+  localStorage.setItem("token", jwt);
+}
+
+export function loginWithJwt(jwt) {
+  localStorage.getItem("token");
+}
+
+export function logout() {
+  localStorage.removeItem("token");
+}
+
+export function getjwt() {
+  return localStorage.getItem("token");
+}
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem("token");
+    return jwt_decode(jwt);
+  } catch (ex) {
+    //console.log("test4");
+    return null;
+  }
+}
+
+const auth  = {
+  login,
+  logout,
+  getCurrentUser,
+  loginWithJwt,
+  getjwt,
+};
+
+export default auth;
---
+++ client/src/services/registrationService.jsx
@@ -0,0 +1,13 @@
+import http from "./httpServices";
+
+const apiEndpoint = "/users";
+
+
+
+export function register(user) {
+  return http.post(apiEndpoint, {
+    email: user.username,
+    password: user.password,
+    name: user.name,
+  });
+}
