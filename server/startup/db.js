29dc55ed4f1cf61f369e9c81cc099f34b29b94a7
Smita Raj <smita.raj12@gmail.com>
2022-03-12 16:31:47 -0800

    make it better

--- server/startup/db.js
+++ server/startup/db.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const db= mysql.createConnection({
   host: "localhost",
   user:"root",
-  password:"epicodus",
+  password:"Ragu@2905",
   database: "timeentrydatabase"
 });
 
